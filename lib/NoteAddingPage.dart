e7d4d1a2001e9af571d429270bf39270067a2709
fahricandurucan <fahricandurucan@icloud.com>
2023-03-29 13:52:46 +0300

    started to the design on NoteAddingPage

--- lib/NoteAddingPage.dart
+++ lib/NoteAddingPage.dart
@@ -13,10 +13,10 @@ class NoteAddingPage extends StatefulWidget {
 class _NoteAddingPageState extends State<NoteAddingPage> {
 
   var db = DatabaseHelper();
-
   int dropDownValue = 1;
-
   late List<Category> allCategory = [];
+  static var _oncelik = ["Düşük","Orta","Yüksek"];
+  String dropDownOncelikValue = _oncelik[0];
 
 @override
   void initState() {
@@ -33,6 +33,7 @@ class _NoteAddingPageState extends State<NoteAddingPage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text("Yeni Not"),
       ),
@@ -44,21 +45,60 @@ class _NoteAddingPageState extends State<NoteAddingPage> {
               children: [
                 Text("Kategori:",style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),),
                 SizedBox(
-                  width: 50,
+                  width: 30,
                 ),
-
                 allCategory.length<=0 ? CircularProgressIndicator() :
-                DropdownButton(
-                    value: dropDownValue,
-                    items: kategoriItems(),
-                    onChanged: (value){
-                      setState(() {
-                        dropDownValue = value!;
-                      });
-                    }
+                Container(
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton(
+                        value: dropDownValue,
+                        items: kategoriItems(),
+                        onChanged: (value){
+                          setState(() {
+                            dropDownValue = value!;
+                          });
+                        }
+                    ),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 6),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.redAccent,width: 2),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
                 ),
               ],
             ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Not başlığını giriniz",
+                labelText: "Başlık",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              maxLines: 4,
+              decoration: InputDecoration(
+                hintText: "Not içeriğini giriniz",
+                labelText: "İçerik",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(
+              height: 30,
+            ),
+            oncelikDurumu(),
+            SizedBox(
+              height: 30,
+            ),
+            buttons(),
           ],
         ),
       ),
@@ -77,8 +117,72 @@ class _NoteAddingPageState extends State<NoteAddingPage> {
     List<DropdownMenuItem> kategoriItems(){
       return allCategory.map((kategori) => DropdownMenuItem(
           value: kategori.categoryID,
-          child: Text(kategori.categoryName),
+          child: Text(kategori.categoryName,style: TextStyle(fontSize: 20),),
       )).toList();
     }
+
+  oncelikDurumu() {
+    return Row(
+      children: [
+        Text("Öncelik:",style: TextStyle(fontSize: 28,fontWeight: FontWeight.bold),),
+        SizedBox(
+          width: 30,
+        ),
+        allCategory.length<=0 ? CircularProgressIndicator() :
+        Container(
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+                value: dropDownOncelikValue,
+                items: _oncelik.map((e){
+                  return DropdownMenuItem(value : e,child: Text(e,style: TextStyle(fontSize: 20),));
+                }).toList(),
+                onChanged: (value){
+                  setState(() {
+                    dropDownOncelikValue = value!;
+                  });
+                }
+            ),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 6),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.redAccent,width: 2),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+        ),
+      ],
+    );
+  }
+
+  buttons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Padding(
+            padding: EdgeInsets.only(left: 8),
+          child: TextButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            child: Text("KAYDET",style: TextStyle(fontSize: 25,color: Colors.green),),
+            style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll<Color>(Colors.grey.shade300),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            child: Text("VAZGEÇ",style: TextStyle(fontSize: 25,color: Colors.red),),
+            style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll<Color>(Colors.grey.shade300),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 
 }
